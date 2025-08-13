c9f39811c9e997b144ea14bd7fee4ad6037c0a60
Justin Donofrio <justin026@protonmail.com>
2025-08-13 12:47:48 -0400

    first commit

---
+++ src/portable.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+from ringcentral_mass_sms.gui import main
+
+if __name__ == '__main__':
+    main()
---
+++ src/ringcentral_mass_sms/gui.py
@@ -0,0 +1,20 @@
+import os
+import sys
+import threading
+from PyQt6.QtCore import QTranslator
+from PyQt6.QtGui import QIcon
+from PyQt6.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QStyle
+from .qt.mainui import MainWindow
+
+def main():
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+
+    app.exec()
+
+    os._exit(0)
+
+
+if __name__ == '__main__':
+    main()
